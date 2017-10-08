6bd9d35d8d4903480668d0a0d2c69428807788de
BorisHo <BHo@student.neumont.edu>
2017-10-07 18:03:00 -0600

    Player Class and Board Class
    Created Player and Board Class

---
+++ Nim/Nim/BoardClass/Board.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nim.BoardClass
+{
+    public class Board
+    {
+        private int totalPieces;
+
+        public int TotalPieces
+        {
+            get { return totalPieces; }
+            set { totalPieces = value; }
+        }
+
+        private int[][] boardState;
+
+        public int[][] BoardState
+        {
+            get { return boardState; }
+            set { boardState = value; }
+        }
+
+        public bool TakeAwayPiece(int row, int col)
+        {
+
+            return false;
+        }
+    }
+
+}
